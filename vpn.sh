5439e0bdb0fa6aa449a2b718d4079fb8286212f3
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-01-17 13:34:54 -0500

    Delete vpn.sh

--- vpn.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/zsh
-x=$(systemctl is-active --quiet openvpn-client@tsvpn.service && echo 1 || echo 0)
-if [ $x -eq 1 ]; then 
-  echo $1 | sudo -S -k systemctl stop openvpn-client@tsvpn.service && echo "\nVPN is now DEAD"
-else
-  echo $1 | sudo -S -k systemctl start openvpn-client@tsvpn.service && echo "\nVPN is now ACTIVE"
-fi
-
-# /home/navinn/Code/vpn.sh ravindarran6543
